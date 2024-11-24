a2b485f183c279c9578fe6df7a6fb11a8478d5b6
SuryaZura <cool.gemilang@gmail.com>
2024-11-25 01:27:18 +0800

    main

--- app/src/main/java/com/example/utsgrafikkom/MyGLSurfaceView.java
+++ app/src/main/java/com/example/utsgrafikkom/MyGLSurfaceView.java
@@ -5,7 +5,7 @@ import android.opengl.GLSurfaceView;
 
 public class MyGLSurfaceView extends GLSurfaceView {
 
-    private final MyGLRenderer renderer;
+    private final MyGLRenderer Renderer;
 
     public MyGLSurfaceView(Context context) {
         super(context);
@@ -14,10 +14,10 @@ public class MyGLSurfaceView extends GLSurfaceView {
         setEGLContextClientVersion(2);
 
         // Create a new Renderer instance
-        renderer = new MyGLRenderer();
+        Renderer = new MyGLRenderer();
 
         // Set the Renderer for drawing on the GLSurfaceView
-        setRenderer(renderer);
+        setRenderer(Renderer);
 
         // Render the view only when there is a change in the drawing data
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
